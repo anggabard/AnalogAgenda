dc293913bb8563c9f726186d118c268ea61d9b78
gabrielangel.ardeleanu <gabrielangel.ardeleanu@gmail.com>
2025-07-01 22:00:33 +0300

    removed api

--- analogagenda.client/src/proxy.conf.js
+++ analogagenda.client/src/proxy.conf.js
@@ -6,14 +6,11 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/api/**",
+      "/**",
     ],
     target,
     secure: false,
-    changeOrigin: true,
-    pathRewrite: {
-      "^/api": ""
-    }
+    changeOrigin: true
   }
 ]
 
