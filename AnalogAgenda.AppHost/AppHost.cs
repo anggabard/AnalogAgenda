623f54ab6fc5970555c1268083eab33d13834584
Angel Ardeleanu <angel.ardeleanu@visma.com>
2025-10-22 17:13:09 +0100

    Add dynamic port

--- AnalogAgenda.AppHost/AppHost.cs
+++ AnalogAgenda.AppHost/AppHost.cs
@@ -7,7 +7,15 @@ var backend = builder.AddProject<Projects.AnalogAgenda_Server>("analogagenda-ser
 var frontend = builder.AddNpmApp("analogagenda-client", "../analogagenda.client")
     .WithReference(backend)
     .WaitFor(backend)
-    .WithHttpEndpoint()
-    .WithExternalHttpEndpoints();
+    .WithHttpEndpoint(name: "http");
+
+frontend.WithEnvironment((context) =>
+{
+    var endpoint = frontend.GetEndpoint("http");
+    var targetPort = endpoint.Property(EndpointProperty.TargetPort);
+    context.EnvironmentVariables.Add("PORT", ReferenceExpression.Create($"{targetPort}"));
+});
+
+frontend.WithExternalHttpEndpoints();
 
 builder.Build().Run();
