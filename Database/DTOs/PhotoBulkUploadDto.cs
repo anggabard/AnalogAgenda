c955db3100dd10d29ee20968899f833de60178c7
Angel Ardeleanu <angel.ardeleanu@visma.com>
2025-11-10 19:18:49 +0200

    increased version

--- Database/DTOs/PhotoBulkUploadDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Database.DTOs;
-
-public class PhotoCreateDto
-{
-    public required string FilmId { get; set; }
-    public required string ImageBase64 { get; set; }
-    public int? Index { get; set; } // Optional index (0-999). If not provided, next available index is used
-}
