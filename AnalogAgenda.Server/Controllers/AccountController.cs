719aad73d1db7e4dfbdf802f61a11f2407eebc64
Angel Ardeleanu <angel.ardeleanu@visma.com>
2026-04-23 17:16:19 +0300

    hotfix user login

--- AnalogAgenda.Server/Controllers/AccountController.cs
+++ AnalogAgenda.Server/Controllers/AccountController.cs
@@ -17,7 +17,7 @@ public class AccountController(IDatabaseService database) : ControllerBase
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginDto login)
     {
-        var users = await database.GetAllAsync<UserEntity>(user => user.Email.Equals(login.Email, StringComparison.InvariantCultureIgnoreCase));
+        var users = await database.GetAllAsync<UserEntity>(user => user.Email == login.Email.ToLowerInvariant());
 
         if (users.Count == 0) return Unauthorized("Bad creds");
 
